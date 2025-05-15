d50e99f57cbcee14a524d15d8109f7b7c6a18aa1
Candas Kat <hrisohest@gmail.com>
2025-05-15 21:37:33 +0200

    Add DTO classes for orders and order products

---
+++ src/main/java/com/candas/order_management/dto/CommandesDTO.java
@@ -0,0 +1,7 @@
+package com.candas.order_management.dto;
+
+import com.candas.order_management.models.Commandes;
+
+public class CommandesDTO {
+    public Commandes.OrderStatus status;
+}
---
+++ src/main/java/com/candas/order_management/dto/CommandesProduitsDTO.java
@@ -0,0 +1,7 @@
+package com.candas.order_management.dto;
+
+public class CommandesProduitsDTO {
+    public Long commande_id;
+    public Long produit_id;
+    public int quantite;
+}
